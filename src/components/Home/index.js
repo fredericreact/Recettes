0656a282fb9e1c0877a326b8f6eb784dcf7e8299
unknown <fredericreact@gmail.com>
2022-10-04 18:13:57 +0800

    add login component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -13,17 +13,17 @@ console.log(list);
 return (
 
 <div className='home'>
-    <p>bienvenue</p>
+    <p>Bienvenue</p>
     <div className='cards-list'>
 
     {
         list.map((cardObject)=>(
             <div key={cardObject.id} className='card'>
-            <img className="card-image" src={cardObject.image}/>
+            <img className="card-image" src={cardObject.image} alt="illustration"/>
             <div className='card-infos'>
             <h3 className='card-title'>{cardObject.title}</h3>
-            <p className='card-difficulty'>difficulte : {cardObject.difficulty}</p>
-            <Link to={cardObject.slug} className='active'>lien recette</Link>
+            <p className='card-difficulty'>Difficulte : {cardObject.difficulty}</p>
+            <Link to={cardObject.slug} className='active'>Lien recette</Link>
             </div>
         </div>
 
